1a4bb2fbf5110e2a266c4f7e93bcc83e9af455a8
FishPy Project <fishpyproject.apps@gmail.com>
2025-04-06 12:29:59 -0300

    Update table.py

--- storage/table.py
+++ storage/table.py
@@ -1,8 +1,11 @@
 class Table:
   #TODO: class docstring
   
-  def __init__(self):
-    #TODO: initializer method docstring
-    self._data = {}
-    self._hiden = {}
-    
+    def __init__(self, data = None):
+      #TODO: initializer method docstring
+        if data == None:
+            self._data = {}
+        else:
+            #TODO: type(data) validation
+            self._data = data
+        self._hiden = {}
